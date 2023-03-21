9742d874ccc253b39705a510b678a30354d4b0b8
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2023-03-20 21:27:38 -0300

    display books from js script

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+const cardsContainer = document.querySelector(".cards-container");
+
 class Book {
-  constructor(title = '', author = '', numPages = 0, hasRead = false) {
+  constructor(title = "", author = "", numPages = 0, status = false) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
-    this.hasRead = hasRead;
+    this.status = status;
   }
 }
 
 const myLibrary = [
-  new Book('harry potter', 'JK Rolling', 100, false),
-  new Book('Elantris', 'Unknown', 500, true),
-  new Book('The Handsmaid\'s tale', 'Margaret Atwood', 300, true),
+  new Book("harry potter", "JK Rolling", 100, false),
+  new Book("Elantris", "Unknown", 500, true),
+  new Book("The Handmaid's tale", "Margaret Atwood", 300, true),
 ];
 
-
-function addBookToLibrary(title, author, numPages, hasRead) {
-  const newBook = new Book(title, author, numPages, hasRead);
-  myLibrary.push(newBook);
-}
+// function addBookToLibrary(title, author, numPages, status) {
+//   const newBook = new Book(title, author, numPages, status);
+//   myLibrary.push(newBook);
+// }
 
 function displayLibrary() {
-  console.log(myLibrary);
+  myLibrary.forEach((book) => {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book-card");
+
+    const bookProperties = Object.keys(book);
+
+    bookProperties.forEach((property) => {
+      const element = document.createElement("p");
+      element.innerHTML = book[property];
+      bookElement.appendChild(element);
+    });
+
+    cardsContainer.appendChild(bookElement);
+  });
 }
+
+window.addEventListener("load", displayLibrary);
